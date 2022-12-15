1b622e127ab9d5d6c14bdaa2da6e9071f582ae72
Riobener <v.shevchenko@energochain.ru>
2022-12-15 06:52:20 +0300

    Fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,28 +1,18 @@
 import requests
-import socket
 
-hostname = socket.gethostname()
-
-ip_address = socket.gethostbyname(hostname)
-
-api = f'http://{ip_address}:8081/users'
-
-def test_health():
-    response = requests.get(f'{api}/health', verify=False)
-    assert response.status_code == 200
+api = 'http://host.docker.internal:8081/users'
 
 def test_user_empty_get():
     id = '7bd047cb-a57e-412c-9d83-81c50e3e3902'
-    response = requests.get(f'{api}/{id}', verify=False)
+    response = requests.get(f'{api}/{id}')
     assert response.status_code == 200
-    assert len(response.json()) == 0
 
 def test_user_save_and_get():
     parameters = "?name=Benjamin&departmentId=7bd047cb-a57e-412c-9d83-81c50e3e3902"
-    response = requests.post(f'{api}/{parameters}', verify=False)
+    response = requests.post(f'{api}/{parameters}')
     assert response.status_code == 200
     assert response.json().get('name') == 'Benjamin'
     createdUserId = response.json().get('id')
-    responseUser = requests.get(f'{api}/{createdUserId}', verify=False)
+    responseUser = requests.get(f'{api}/{createdUserId}')
     assert responseUser.status_code == 200
     assert responseUser.json().get('name') == 'Benjamin'
