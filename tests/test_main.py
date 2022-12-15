782462c4ae8ad5cb103806dcb2942f65089d4c9d
Riobener <v.shevchenko@energochain.ru>
2022-12-15 05:29:15 +0300

    Fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ import requests
 api = 'http://localhost:8081/users'
 
 def test_health():
-    response = requests.get(f'{api}/health')
+    response = requests.get(f'{api}/health', verify=False)
     assert response.status_code == 200
 
 def test_user_empty_get():
@@ -14,10 +14,10 @@ def test_user_empty_get():
 
 def test_user_save_and_get():
     parameters = "?name=Benjamin&departmentId=7bd047cb-a57e-412c-9d83-81c50e3e3902"
-    response = requests.post(f'{api}/{parameters}')
+    response = requests.post(f'{api}/{parameters}', verify=False)
     assert response.status_code == 200
     assert response.json().get('name') == 'Benjamin'
     createdUserId = response.json().get('id')
-    responseUser = requests.get(f'{api}/{createdUserId}')
+    responseUser = requests.get(f'{api}/{createdUserId}', verify=False)
     assert responseUser.status_code == 200
     assert responseUser.json().get('name') == 'Benjamin'
