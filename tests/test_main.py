d9c3cbb306d6793dfa7bb038f486e53f348b716d
cypermen <41022014+cypermen@users.noreply.github.com>
2022-12-15 23:41:14 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,23 +1,25 @@
 import requests
 
-api = 'http://localhost:8081/users'
+api = 'http://localhost:8081/book'
+
+
 
 def test_user_empty_get():
     s = requests.Session()
     id = '7bd047cb-a57e-412c-9d83-81c50e3e3902'
-    response = s.get(f'{api}/{id}')
+    response = s.get(f'{api}/find/{id}')
+    print(response.status_code)
     assert response.status_code == 200
 
 
 def test_user_save_and_get():
     s = requests.Session()
-    parameters = "?name=Benjamin&departmentId=7bd047cb-a57e-412c-9d83-81c50e3e3902"
-    response = s.post(f'{api}/{parameters}')
+    parameters = "?title=cypermen&authorId=7bd047cb-a57e-412c-9d83-81c50e3e3902"
+    response = s.post(f'{api}/add/{parameters}')
+    print(response.status_code)
     assert response.status_code == 200
-    assert response.json().get('name') == 'Benjamin'
-    createdUserId = response.json().get('id')
-    responseUser = s.get(f'{api}/{createdUserId}')
-    assert responseUser.status_code == 200
-    assert responseUser.json().get('name') == 'Benjamin'
-
-
+    assert response.json().get('title') == 'cypermen'
+    createdBookId = response.json().get('id')
+    responseBook = s.get(f'{api}/find/{createdBookId}')
+    assert responseBook.status_code == 200
+    assert responseBook.json().get('title') == 'cypermen'
